dfb048247fc11132350aa18b7a2a1ceea745acc9
khalidjuna <khalidjunata@gmail.com>
2024-10-09 02:51:12 +0700

    change local

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/stressmap/',
+  base: '/stressmap/',
   plugins: [react(), VitePWA({
     strategies: 'injectManifest',
     srcDir: 'src',
